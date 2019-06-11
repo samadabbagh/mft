bfc5b05e2f3816d4be7666bcdb13f91105cf7cef
samadabbagh <sama.dabbagh@yahoo.com>
2019-06-11 06:36:05 -0700

    tamrin1

---
+++ mft-first-section/tamriin 1.py
@@ -0,0 +1,42 @@
+
+
+text = """The most basic data structure in Python is the sequence. 
+Each element of a sequence is assigned a number - its position or index. 
+The first index is zero, the second index is one, and so forth.
+Python has six built-in types of sequences, but the most common 
+ones are lists and tuples, which we would see in this tutorial.
+There are certain things you can do with all the sequence types. 
+These operations include indexing, slicing, adding, multiplying, and 
+checking for membership. In addition, Python has built-in functions for 
+finding the length of a sequence and for finding its largest 
+and smallest elements."""
+
+
+print(len(text))
+
+print(set(text))
+
+text1 = text.split()
+print(text1)
+
+
+
+text = """The most basic data structure in Python is the sequence. 
+Each element of a sequence is assigned a number - its position or index. 
+The first index is zero, the second index is one, and so forth.
+Python has six built-in types of sequences, but the most common 
+ones are lists and tuples, which we would see in this tutorial.
+There are certain things you can do with all the sequence types. 
+These operations include indexing, slicing, adding, multiplying, and 
+checking for membership. In addition, Python has built-in functions for 
+finding the length of a sequence and for finding its largest 
+and smallest elements."""
+
+
+print(len(text))
+
+print(set(text))
+
+text1 = text.split()
+print(text1)
+
