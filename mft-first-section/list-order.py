894a56d1ceca77fc600ff53d5c2efb15875596fd
sama <sama.dabbagh@yahoo.com>
2019-06-19 20:51:02 +0430

    order tamrin

---
+++ mft-first-section/list-order.py
@@ -0,0 +1,12 @@
+lst = []
+while True:
+
+
+    number = int(input("enter your number :"))
+    list.append(lst, number)
+
+
+    no = input("agr nemikhahid edame dahid n ra vared konid ")
+    if no == "n":
+        break
+print(lst)
